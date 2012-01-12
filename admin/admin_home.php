01781108add9e9652b2d0fe1742a2a7ec2fd6be0
Chris Lynch <github@planetofthepenguins.com>
2012-01-12 23:20:00 +0000

    Continued mods in admin system

--- admin/admin_home.php
+++ admin/admin_home.php
@@ -1,1 +1,11 @@
 <?php
+
+$entries = mysql_query("SELECT * FROM funamble_index");
+
+print '<ul>';
+while($entry = mysql_fetch_assoc($entries)){
+	print '<li>' . $entry['index_id'] . '</li>';
+}
+print '</ul>';
+
+?>
