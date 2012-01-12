7e5057460f9f489425a2294bf60229378df1e9ca
Chris Lynch <github@planetofthepenguins.com>
2012-01-12 23:18:28 +0000

    Bootstrap file to allow start up from index.php or admin.php

---
+++ bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+// Start by making a database connection. No database = no funamble.
+$db = mysql_connect($db_host,$db_user,$db_password) or die('Could not connect to database. No database = No funamble');
+// Now select the fumable db
+mysql_selectdb($db_schema) or die('Could not find schema. No schema = No funamble');
+// Ideally, we would now check to see if the table(s) are there and, if not, create them. If there's time, we can come back and do this.
+?>
