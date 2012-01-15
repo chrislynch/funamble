0224a8e07013b66e0bdb0d318aeadb5bf7df4761
Chris Lynch <github@planetofthepenguins.com>
2012-01-15 23:20:11 +0000

    Themeatic fiddling

--- skins/victorian_pulp/content-bottom.php
+++ skins/victorian_pulp/content-bottom.php
@@ -1,9 +1,12 @@
 		</div>
-		<div class="span-7 last">
-			<!-- Blood Brothers -->
+		<div id="sidebar" class="span-6 last">
+			<!-- The Dark, Blood Brothers -->
+			<h2>Buy Online</h2>
 			<div align="center">
+			<iframe src="http://rcm-uk.amazon.co.uk/e/cm?lt1=_blank&bc1=F5F2D5&IS2=1&bg1=F5F2D5&fc1=000000&lc1=0000FF&t=chrislynch&o=2&p=8&l=as4&m=amazon&f=ifr&ref=ss_til&asins=1905692374" style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>
 			<iframe src="http://rcm-uk.amazon.co.uk/e/cm?lt1=_blank&bc1=F5F2D5&IS2=1&bg1=F5F2D5&fc1=000000&lc1=0000FF&t=chrislynch&o=2&p=8&l=as4&m=amazon&f=ifr&ref=ss_til&asins=B004TGT8AE" style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>
-			<iframe src="http://rcm-uk.amazon.co.uk/e/cm?lt1=_blank&bc1=F5F2D5&IS2=1&bg1=F5F2D5&fc1=000000&lc1=0000FF&t=chrislynch&o=2&p=8&l=as4&m=amazon&f=ifr&ref=ss_til&asins=B004TGT8AE" style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>
+			<iframe src="http://rcm-uk.amazon.co.uk/e/cm?lt1=_blank&bc1=F5F2D5&IS2=1&bg1=F5F2D5&fc1=000000&lc1=0000FF&t=chrislynch&o=2&p=8&l=as4&m=amazon&f=ifr&ref=ss_til&asins=190580816X" style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>
+			<iframe src="http://rcm-uk.amazon.co.uk/e/cm?lt1=_blank&bc1=&bc1=F5F2D5&IS2=1&bg1=F5F2D5&fc1=000000&lc1=0000FF&t=chrislynch&o=2&p=8&l=as4&m=amazon&f=ifr&ref=ss_til&asins=1897290012" style="width:120px;height:240px;" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>
 			</div>
 			<h2>Twitterati</h2>
 			<?php include 'widgets/lifestream.php'; ?>
--- skins/victorian_pulp/content-top.php
+++ skins/victorian_pulp/content-top.php
@@ -1,4 +1,4 @@
 <div class="container">
 	<div class="span-24 last">
-		<div class="span-16">
+		<div class="span-16 append-1">
 		<h1>Planet of the Penguins</h1>
